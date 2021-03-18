6d97782d13da1ed2be07a4290fe89594edcce591
Soultan Hatsijev <soultan_98@hotmail.com>
2021-03-18 17:20:23 +0100

    Création des view et viewmodel profil et information de profil

---
+++ Bibliotheque/Bibliotheque/ViewModels/ProfileInformationsViewModel.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Bibliotheque.EntityFramework.Services.Repositories;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotheque.UI.ViewModels
+{
+    public class ProfileInformationsViewModel : BindableBase, INavigationAware
+    {
+        private readonly ILibraryRepository m_Repository;
+        private readonly IMapper m_Mapper;
+
+        private IRegionNavigationService m_Navigation;
+
+        public ProfileInformationsViewModel(ILibraryRepository repository, IMapper mapper)
+        {
+            m_Repository = repository ??
+                throw new ArgumentNullException(nameof(repository));
+            m_Mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
